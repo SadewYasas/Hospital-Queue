7b7f96f1293a9d90ffe381a7e0b9b458715f8559
Sadew <Sadewyasas11@gmail.com>
2026-02-23 19:11:52 +0530

    Initial Commit

---
+++ HospitalQueue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HospitalQueue
+//
+//  Created by COBSCCOMP24.2P-075 on 2026-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HospitalQueue/HospitalQueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  HospitalQueueApp.swift
+//  HospitalQueue
+//
+//  Created by COBSCCOMP24.2P-075 on 2026-02-23.
+//
+
+import SwiftUI
+
+@main
+struct HospitalQueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
